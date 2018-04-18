a0318a86c6855472457a271a06c79b239437a2df
G00347900 <35769360+G00347900@users.noreply.github.com>
2018-04-18 14:17:40 +0100

    Added Irish and maths

--- js/grader.js
+++ js/grader.js
@@ -1,24 +1,68 @@
 function gradeResult() {
 	
-	var myResult = parseInt(document.getElementById("value1").value);
-	var grade;
+	var myEnglishResult = parseInt(document.getElementById("value1").value);
+	var myIrishResult = parseInt(document.getElementById("value1").value);
+	var myMathsResult = parseInt(document.getElementById("value1").value);
 	
-	if (!myResult){
+	var Englishgrade;
+	var Irishgrade;
+	var Mathsgrade;
+	
+	
+	if (!myEnglishResult){
 		alert("Please enter a result");
 	
 	}
 	else {
-		if (myResult > 90) {
-			grade = "A" ;
-		} else if (myResult > 80) {
-			grade = "B";
+		if (myEnglishResult > 90) {
+			Englishgrade = "A" ;
+		} else if (myEnglishResult > 80) {
+			Englishgrade = "B";
 		} else {
-			grade ="F";
+			Englishgrade ="F";
 		}
 		
-		var message = "Your result of" + myResult + "get you a grade" + grade;
-		console.log(message);
-		document.getElementById("resultsentence").innerHTML = message;
+	if (!myEnglishResult){
+		alert("Please enter a result");
+	
+	}
+	else {
+		if (myIrishResult > 90) {
+			Irishgrade = "A" ;
+		} else if (myIrishResult > 80) {
+			Irishgrade = "B";
+		} else {
+			Irishgrade ="F";
+		}
+		
+	if (!myIrishResult){
+		alert("Please enter a result");
+	
+	}
+	else {
+		if (myMathsResult > 90) {
+			Mathsgrade = "A" ;
+		} else if (myMathsResult > 80) {
+			Mathsgrade = "B";
+		} else {
+			Mathsgrade ="F";
+		}
+	}
+	}
 	}
 }
+		
+		
+		var EnglishMessage = "Your result of" + myEnglishResult + "get you a grade" + EnglishGrade;
+		console.log(EnglishMessage);
+		document.getElementById("resultsentence").innerHTML = EnglishMessage;
+		
+		var IrishMessage = "Your result of" + myIrishResult + "get you a grade" + IrishGrade;
+		console.log(IrishMessage);
+		document.getElementById("resultsentence").innerHTML = IrishMessage;
+		
+		var MathsMessage = "Your result of" + myMathsResult + "get you a grade" + MathsGrade;
+		console.log(MathsMessage);
+		document.getElementById("resultsentence").innerHTML = MathsMessage;
+	
 
